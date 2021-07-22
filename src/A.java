1cb73a1505acc190d7892b2f940ffef1ff53bf99
QSP <QSP@QSP>
2021-07-22 15:20:24 +0530

    Hi

---
+++ src/A.java
@@ -0,0 +1,9 @@
+
+public class A {
+
+	public static void main(String[] args) {
+		int a=10;
+		System.out.println(a);
+	}
+
+}
---
+++ src/demo/Test1.java
@@ -0,0 +1,29 @@
+package demo;
+
+public class Test1 {
+	
+	public static void add() {
+		int a=10;
+		int b=20;
+		int c=a+b;
+		System.out.println(c);
+	}
+	
+	public static void sub() {
+		int a=100;
+		int b=50;
+		int c=a-b;
+		System.out.println(c);
+	}
+
+	public static void main(String[] args) {
+		System.out.println("Hello");
+		add();
+		System.out.println("bye");
+		sub();
+		System.out.println("GE");
+		
+
+	}
+
+}
