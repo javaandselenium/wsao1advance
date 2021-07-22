d30b97e255218271361886c214ecf30d3305ff6d
QSP <QSP@QSP>
2021-07-22 15:26:24 +0530

    Hi

--- src/A.java
+++ src/A.java
@@ -1,9 +1,14 @@
 
 public class A {
+	public void demo() {
+		System.out.println("hello");
+	}
 
 	public static void main(String[] args) {
 		int a=10;
 		System.out.println(a);
+		A a1=new A();
+		a1.demo();
 	}
 
 }
